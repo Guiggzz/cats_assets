c66a13f3319799613c66124d9e6953892a39a6a5
guigz <guillaume.breon@gmail.com>
2024-03-11 12:12:30 +0100

    php

---
+++ website-menu-03/website-menu-03/header.php
@@ -0,0 +1,77 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <link rel="stylesheet" href="css/style_index.css">
+
+    <link rel="stylesheet" href="css/main.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel="icon" href="images/logocats.png" type="image/x-icon">
+
+    <title>Page d'accueil</title>
+</head>
+
+<body>
+
+    <div class="backgroundnav">
+        <div class="site-mobile-menu">
+            <div class="site-mobile-menu-header">
+                <div class="site-mobile-menu-close mt-3">
+                    <span class="icon-close2 js-menu-toggle"></span>
+                </div>
+            </div>
+            <div class="site-mobile-menu-body"></div>
+        </div>
+
+        <header class="site-navbar" role="banner">
+
+            <div class="container">
+                <div class="row align-items-center">
+
+                    <div class="col-10 col-md-11 d-none d-xl-block">
+                        <nav class="site-navigation position-relative text-right" role="navigation">
+                            <div class="col-10 col-xl-10">
+                                <h1 class="mb-0 site-logo"><a href="index.html"><img src="images/cats.png" style="width: 610px; border-radius: 5px; text-align: center;" alt="Cats" class="text-white mb-0"></a></h1>
+                            </div>
+                            <ul class="site-menu js-clone-nav mr-auto d-none d-lg-block">
+                                <div class="col-9 col-xl-9">
+                                    <h1 class="mb-0 site-logo"><a href="index.html"><img src="images/cats.png" style="width: 210px; border-radius: 5px; text-align: center;" alt="Cats" class="text-white mb-0 burger-image"></a></h1>
+                                </div>
+
+                                <li class="active"><a href="index.html"><span>Page d'accueil</span></a></li>
+                                <li class="has-children">
+                                    <a><span>Nos services</span></a>
+                                    <ul class="dropdown arrow-top">
+                                        <li><a href="servicesTP.html">Travaux publics</a></li>
+                                        <li><a href="servicesbat.html">Bâtiments</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="listings.html"><span>Moyens</span></a></li>
+                                <li><a href="about.html"><span>Certifications</span></a></li>
+                                <li><a href="blog.html"><span>Partenaires</span></a></li>
+                                <li><a href="contact.html"><span>Au fil du temps</span></a></li>
+                                <li><a href="contact.html"><span>Contact</span></a></li>
+                            </ul>
+                        </nav>
+                    </div>
+
+                    <div class="d-inline-block d-xl-none ml-md-0 mr-auto py-3" style="position: relative; top: 3px;"><a href="#" class="site-menu-toggle js-menu-toggle text-black"><span class="icon-menu h3"></span></a></div>
+                    <img src="images/logocats.png" class="logopetit">
+                </div>
+            </div>
+    </div>
+    </div>
+    </header>
---
+++ website-menu-03/website-menu-03/index.php
@@ -0,0 +1,186 @@
+<head>
+  <?php require_once 'header.php'; ?>
+</head>
+<div class="parent">
+  <div class="card1">
+    <div class="texteintro">
+      <h1><b>CANALISATIONS <br>TRAVAUX SPÉCIAUX</b></h1>
+      <p>Bâtiments et travaux publics
+      <p>
+    </div>
+  </div>
+  <div class="photointro">
+    <iframe width="100%" height="350px" src="https://www.youtube.com/embed/lIFiwX58du0?si=7nD86qlMuvZ3Dlal&amp;autoplay=1&mute=1&controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+  </div>
+</div>
+
+<div class="grid-containerr">
+  <h2 class="titresection"><b>Services : </b></h2>
+  <div class="cardservices">
+    <img src="images/grue.png" alt="Grue">
+    <h3>Bâtiment intérieur / extérieur</h3>
+    <div class="descriptionservices">
+      <ul>
+        <li>Désamiantage de toiture en fibro ciment par le dessus</li>
+        <li>Désamiantage de toiture en fibro ciment par le dessous</li>
+        <li>Désamiantage de plaques de caniveaux technique, <br>faux plafonds et conduits en fibro ciment</li>
+        <li>Retrait, pose et réalisation de fosse septique</li>
+        <li>Terrassement</li>
+      </ul>
+    </div>
+  </div>
+  <div class="cardservices">
+    <img src="images/tracto.png" alt="Grue">
+    <h3>Travaux publics</h3>
+    <div class="descriptionservices">
+      <ul>
+        <li>Pose de canalisations d’adduction d’eau</li>
+        <li>Pose de de canalisations gravitaires eaux usées, eaux pluviales</li>
+        <li>Installation d’assainissement autonome</li>
+        <li>Installation de relevage d’eaux usées</li>
+        <li>Travaux urbains</li>
+        <li>Désamiantage de canalisations en fibro ciment en<br> sous-section 3 (retrait en lieu et place) et
+          <br>sous-section
+          4 (interventions ponctuelles : fuite, reprise de branchement)
+        </li>
+        <li>Retrait de revêtement de chaussée amiantés</li>
+      </ul>
+    </div>
+  </div>
+</div>
+
+<div class="grid-container">
+  <h2 class="titresection" style="padding-top: 50px;"><b>Associés : </b></h2>
+  <div class="card">
+    <img src="images/jerome.jpg" alt="Image 1">
+    <div class="overlay">
+      Gérant
+    </div>
+    <div class="description">BREON<br>Jérôme </div>
+  </div>
+  <div class="card">
+    <img src="images/jerome.jpg" alt="Image 2">
+    <div class="overlay">Associée</div>
+    <div class="description">BREON<br>Audrey</div>
+  </div>
+  <div class="card">
+    <img src="images/guillaume.jpg" alt="Image 3">
+    <div class="overlay">Associé</div>
+    <div class="description">CLEMENT<br>Guillaume</div>
+  </div>
+  <div class="card">
+    <img src="images/benji.webp" alt="Image 4">
+    <div class="overlay">Principal chef de chantier</div>
+    <div class="description">LEGUERCH<br>Benjamin</div>
+  </div>
+</div>
+</div>
+
+<div class="cardo">
+  <h2 class="titresection" style="padding-top: 50px;"><b>CaTS en données c'est :</b><br><br></h2>
+  <div class="counter-container">
+    <div class="counter">
+      <h3>Années d'expériences</h3>
+      <div class="value" id="years">0</div>
+    </div>
+    <div class="counter">
+      <h3>Salariés</h3>
+      <div class="value" id="employees">0</div>
+    </div>
+    <div class="counter">
+      <h3>Chantiers réalisés</h3>
+      <div class="value" id="projects">0</div>
+    </div>
+  </div>
+</div>
+
+<h2 class="titresection" style="padding-top: 50px;"><b>Nos partenaires :</b></h2>
+<div class="slider">
+  <div class="slide-track">
+    <div class="slide">
+      <img src="images\partenaires\Aude-le-departement-partenaire-de-l_entreprise-cats-narbonne.png">
+    </div>
+    <div class="slide">
+      <img src="images\partenaires\BRL-exploitation-partenaire-de-l_entreprise-cats-narbonne.png" style="height: 120%;" alt="Image 2">
+    </div>
+    <div class="slide">
+      <img src="images\partenaires\communauté-de-commune-de-la-vallée-de-lhérault-partenaire-de-l_entreprise-cats-narbonne.jpg">
+    </div>
+    <div class="slide">
+      <img src="images\partenaires\corbières-salanque-méditérranée-partenaire-de-l_entreprise-cats-narbonne.png">
+    </div>
+    <div class="slide">
+      <img src="images\partenaires\Le_Grand_Narbonne-partenaire-de-l_entreprise-cats-narbonne.png" alt="Image 2" style="height: 110%;">
+    </div>
+    <div class="slide">
+      <img src="images\partenaires\Montpellier-méditerranée-métropole-partenaire-de-l_entreprise-cats-narbonne.jpg">
+    </div>
+    <div class="slide">
+      <img src="images\partenaires\Narbonne-partenaire-de-cats-btp.png">
+    </div>
+    <div class="slide">
+      <img src="images\partenaires\regie-des-eaux-Montpellier-partenaire-de-l_entreprise-cats-narbonne.jpg">
+    </div>
+    <div class="slide">
+      <img src="images\partenaires\region.png">
+    </div>
+  </div>
+</div>
+
+<footer class="modern-footer">
+  <div class="footer-form">
+    <h2>Contactez-nous</h2>
+    <form action="https://formspree.io/f/meqyobkz" method="POST" class="contact-form">
+      <div class="form-group">
+        <label for="name">Nom :</label>
+        <input type="text" class="form-control" name="name" required>
+      </div>
+
+      <div class="form-group">
+        <label for="email">Email :</label>
+        <input type="email" class="form-control" name="email" required>
+      </div>
+
+      <div class="form-group">
+        <label for="company">Entreprise/Société :</label>
+        <input type="text" class="form-control" name="company">
+      </div>
+
+      <div class="form-group">
+        <label for="message">Message :</label>
+        <textarea class="form-control" name="message" rows="4" required></textarea>
+      </div>
+
+      <button type="submit" class="btn btn-primary">Envoyer</button>
+    </form>
+  </div>
+  <div class="footer-contact">
+    <h2>Fiche Contact</h2>
+    <h4 class="title">Une question ?</h4>
+    <div class="queries">
+      <a href="tel:0468401923">&#9742; 04 68 40 19 23</a><br>
+      <a href="mailto:contact@cats-btp.fr"><b>&#9993;</b> contact@cats-btp.fr</a><br>
+      <b>&#x1F552; Horaires :</b><br>
+      Du lundi au jeudi - 8h00 à 18h00<br>
+      Le vendredi - 8h00 à 17h00<br><br><br><br>
+    </div>
+    <div class="footer-map">
+      <h2>Google Maps</h2>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2910.5399079579634!2d2.965936077045043!3d43.15618988443272!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12b1ad180f36d25b%3A0xd36e428dd0fe45bc!2s41%20Av.%20Paul%20Sabatier%2C%2011100%20Narbonne!5e0!3m2!1sen!2sfr!4v1705679827412!5m2!1sen!2sfr" width="100%" height="250" style="border:1;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+</footer>
+
+
+
+
+
+
+<script src="js/jquery-3.3.1.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.sticky.js"></script>
+<script src="js/main.js"></script>
+<script src="js/script.js"></script>
+</body>
+
+</html>
